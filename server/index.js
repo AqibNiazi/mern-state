5a9c51c23ef6523b9b70ea49968817dd63d28a91
AqibNiazi <aqibjaved5201@gmail.com>
2026-01-12 12:36:05 +0500

    Backend application created and connected with database

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+// const bodyParser = require("body-parser");
+const database = require("./src/config/mongodb");
+const cors = require("cors");
+const HOST = process.env.HOST;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const cookieParser = require("cookie-parser");
+const allowedOrigins = [
+  "http://localhost:5173",
+  // "https://mern-auth-plum.vercel.app/",
+];
+// Middlewares
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const NodeJsServer = async () => {
+  try {
+    await database();
+    app.listen(PORT, HOST, () => {
+      console.log(`Backend server is running at http://${HOST}:${PORT}`);
+      console.log("Server running");
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+NodeJsServer();
