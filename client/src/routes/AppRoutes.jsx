8cef064f0b38c8efd2c074188229793a876fc54b
AqibNiazi <aqibjaved5201@gmail.com>
2026-01-13 13:30:12 +0500

    Signup Page UI completed

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import AppLayout from "@/layout/AppLayout";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
 import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
 
 const AppRoutes = () => {
   const router = createBrowserRouter([
@@ -22,6 +23,10 @@ const AppRoutes = () => {
         {
           path: "sign-in",
           element: <SignIn />,
+        },
+        {
+          path: "sign-up",
+          element: <SignUp />,
         },
       ],
     },
